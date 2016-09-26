ffec581b7e5d162da3c4a7d0239f4a0d621963cf
mukeshpsycox <mukeshpsycox@gmail.com>
2016-09-26 17:41:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/love', function (req, res) {
+    res.send('Heres a little inspiration to tell your love how you really');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
